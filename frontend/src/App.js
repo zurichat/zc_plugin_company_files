76680d0366e309a0911c66a3d9fb35b2d32d0a50
Edward Osaretin Oboh <edward@oboh.localdomain>
2021-08-28 13:16:48 -0700

    boiler plate setup for zuri chat company files

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/zurichatlogo.svg" />
+        <h2>
+          Team: GALILOE
+        </h2>
+        <h4>
+          Plug-in: COMPANY FILES
+        </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
