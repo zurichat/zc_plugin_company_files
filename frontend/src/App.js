2f7452abd30c16fcb0ee33d3e6745c8de29bfecc
Williams Chukwuebuka Malachy <billmal071@gmail.com>
2021-09-01 23:04:23 +0100

    fix:changed the app.js file setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,27 +7,26 @@ import SearchBar from './components/SearchBar/SearchBar';
 import Activities from './components/Activities/Activities';
 import TotalSpace from './components/TotalSpace/TotalSpace';
 import Upload from './components/Upload/Upload';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App font-lato">
-      <Sidebar/> 
-      <div className="main">
-          <div >Top items and Search Component <SearchBar/></div>
-          <div className="capitalize text-error">recently viewed</div>
-          <button className="bg-success p-2">test</button>
-          <div>
-            <RecentlyViewed/>
-            <Folder/>
-            <Files/>
-          </div>
-          <div>
-            <TotalSpace/>
-            <Activities/>
-            <Upload/>
-         </div>
+    <Router>
+      <div className="flex font-lato bg-bg-lightGrey">
+        <Sidebar />
+        <div className="main">
+          <div className="">Top items and Search Component <SearchBar /></div>
+          <Switch>
+            <Route path="/activities" component={Activities} />
+            <Route path="/recently-viewed" component={RecentlyViewed} />
+            <Route path="/files" component={Files} />
+            <Route path="/folder" component={Folder} />
+            <Route path="/total-space" component={TotalSpace} />
+            <Route path="/upload" component={Upload} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Activities/Activities.js
+++ frontend/src/components/Activities/Activities.js
@@ -1,7 +1,7 @@
 function Activities() {
     return (
         <div>
-            
+            activities
         </div>
     )
 }
