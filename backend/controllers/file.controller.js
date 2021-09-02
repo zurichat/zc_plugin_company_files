89a4ce41f476c5f84ba03cc03c4cfbe33815c797
Williams Chukwuebuka Malachy <billmal071@gmail.com>
2021-09-02 16:01:25 +0100

    feat:file search by is starred property

--- backend/controllers/file.controller.js
+++ backend/controllers/file.controller.js
@@ -13,3 +13,25 @@ exports.fileUpdate = async (req, res) => {
 exports.fileDelete = async (req, res) => {
   
 }
+
+exports.fileSearchByIsStarred = async (req, res) => {
+  // seacrch the database for files that are starred
+  const {stars} = req.query;
+
+  try {
+    // check if stars is a value
+    if (stars) {
+      // search for files that are starred
+      const files = await File.find({isStarred: true});
+      return res.status(200).json({
+        success: true,
+        count: files.length,
+        data: files
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+}
--- backend/routes/file.route.js
+++ backend/routes/file.route.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-const { fileCreate, fileUpdate, fileDetails, fileDelete } = require('../controllers/file.controller');
+const { fileCreate, fileUpdate, fileDetails, fileDelete, fileSearchByIsStarred } = require('../controllers/file.controller');
 
 router.post('/', fileCreate);
+
+// route to search for files if its starred
+router.get('/searchByStars', fileSearchByIsStarred);
 
 router.route('/:id')
   .get(fileDetails)
