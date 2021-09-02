08680c7e832778d3fae43cfdd13aea0405140ca2
Adetunji Favour <58666102+Tunjii10@users.noreply.github.com>
2021-09-02 21:27:01 +0100

    create-new-folder-backend

--- backend/controllers/folder.controller.js
+++ backend/controllers/folder.controller.js
@@ -1,9 +1,27 @@
+const Folder = require('../models/Folder.js') 
+
 exports.folderCreate = async (req, res) => {
+  try{
+    folder = new Folder(req.body);
+    validateError = folder.joiValidate(req.body);
+    // console.log(folder)
+    savedFolder = await folder.save((err, doc) => {
+      if (err) {
+        return console.error(err);
+        console.log('Document insert unsuccesssful!');
+      } else{
+        res.json(doc)
+      }
+    })
+  } catch (e) {
+    console.log(e)
+  }
+    
   
 }
 
 exports.folderDetails = async (req, res) => {
-
+    
 }
 
 exports.folderUpdate = async (req, res) => {
--- backend/models/Folder.js
+++ backend/models/Folder.js
@@ -1,17 +1,38 @@
 const { Schema, model } = require('mongoose');
-
+const mongoose = require('mongoose')
 const FolderSchema = new Schema({
   folderName: {
     type: String,
     trim: true,
     required: true
-  }
-}, { timestamps: true });
+  },
+  description: {
+    type: String,
+    trim: true,
+    default: '',
+  },
+  folderId: {type: Schema.Types.ObjectId, default: mongoose.Types.ObjectId},
+  permissions: {
+    type: String,
+    trim: true,
+    default: '',
+  },
+  isPinned: {type: Boolean, default: false},
+  lastAccessed: { type: Date, default: Date.now() },
+  dateModified: { type: Date, default: Date.now() },
+  dateAdded: { type: Date, default: Date.now() },
+});
 
 FolderSchema.methods.joiValidate = data => {
 	const Joi = require('joi');
 	const schema = Joi.object().keys({
-		folderName: Joi.string().required()
+		folderName: Joi.string().required(),
+    description: Joi.string(),
+    permissions: Joi.string(),
+
+    lastAccessed: Joi.date(),
+    dateModified: Joi.date(),
+    dateAdded: Joi.date()
   }).unknown(true);
   
   return new Promise((resolve, reject) => {
