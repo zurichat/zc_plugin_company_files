2dc51e7c0994e265effc7e26ad82a954f798ef0f
Emmanuel Uchenna <eunithelp@gmail.com>
2021-09-03 00:08:52 +0100

    feat: solve issue #174
    feat: add the file list view. This is visible on you visit the files dashboard

--- frontend/src/components/FilesCard/FilesCard.js
+++ frontend/src/components/FilesCard/FilesCard.js
@@ -1,7 +1,15 @@
+import FilesListView from "../FilesListView/FilesListView"
+
 function FilesCard() {
     return (
         <div>
-            
+					<FilesListView />
+					<FilesListView />
+					<FilesListView />
+					<FilesListView />
+					<FilesListView />
+					<FilesListView />
+					<FilesListView />
         </div>
     )
 }
