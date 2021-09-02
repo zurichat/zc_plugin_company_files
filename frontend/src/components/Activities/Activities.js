1f26640f3e594461298dc257671e6c10bf9f61aa
Williams Chukwuebuka Malachy <billmal071@gmail.com>
2021-09-02 02:22:01 +0100

    fix:width and spacing issues

--- frontend/src/components/Activities/Activities.js
+++ frontend/src/components/Activities/Activities.js
@@ -4,10 +4,10 @@ function Activities() {
     return (
         <div className="bg-bg-white ml-5 flex-auto px-7 py-5">
             <div className="flex ">
-                <div className=" w-2/3">
+                <div className=" w-3/4">
                     activities
                 </div>
-                <div className="w-1/3 h-full">
+                <div className="w-1/4 h-full">
                     <div className="h-2/5">
                         usage meter
                     </div>
--- frontend/src/components/Activities/Collaborators.js
+++ frontend/src/components/Activities/Collaborators.js
@@ -6,7 +6,7 @@ function Collaborators() {
     <div className='mt-10'>
       <div className='flex justify-between mb-8'>
         <p className='text-text-grey capitalize font-bold text-2xl'>collaborators</p>
-        <div className="rounded-full h-2 w-2 p-3 flex items-center justify-center border-collaborators border-solid border-text-navbar text-text-navbar">5</div>
+        <div className="rounded-full h-2 w-2 p-3 flex items-center justify-center border-collaborators border-solid border-text-navbar text-text-navbar mr-8">5</div>
       </div>
       <CollaboratorsList />
       <CollaboratorsList />
