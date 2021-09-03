6bdfd6ada1a82af71e115e8a7550c56240e89e80
Stephen Gbolagade <gbolabyte222@gmail.com>
2021-09-04 00:53:35 +0100

    UPDATE: I complete the Folder List View Component

--- frontend/src/components/FolderAllFileListView/ListViewTable/ListViewTable.jsx
+++ frontend/src/components/FolderAllFileListView/ListViewTable/ListViewTable.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 
-import './ListViewTable.css';
+import TableHeader from '../TableHeader/TableHeader';
+import TableBody from '../TableBody/TableBody';
+import FileBody from '../TableFileBody/FileBody';
+
+
 
 function ListViewTable() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+
+
+return (
+<table>
+    <TableHeader />
+    <TableBody />
+    <TableBody name="Programming File" />
+    <TableBody name="My favourite"/>
+    <TableBody name="Media Folder"/>
+    <FileBody name="My Videos"/>
+    <FileBody />
+    <FileBody name="Programming Podcasts" />
+    <FileBody name="Netflix Downloads"/>
+
+</table>
+);
+
+};
+
+
+export default ListViewTable;
 
 export default ListViewTable;
