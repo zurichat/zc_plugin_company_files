b803210c59019fa18d9e949f6a33d5361c18ee0b
Otunba Marcus <otunbamarcusademola@gmail.com>
2021-09-03 20:17:53 +0100

    Added feat update-file #278

--- backend/controllers/file.controller.js
+++ backend/controllers/file.controller.js
@@ -1,3 +1,6 @@
+const ApiConnection = require("../utils/database.helper");
+const API = new ApiConnection("File");
+
 exports.fileCreate = async (req, res) => {
   
 }
@@ -7,8 +10,27 @@ exports.fileDetails = async (req, res) => {
 }
 
 exports.fileUpdate = async (req, res) => {
+  try {
+  const file = await API.update(req.params.id, req.body);
 
+  if (!file) return res.status(401).json({
+      response: {
+        status: 401,
+        message: "file does not exist",
+      }
+    });
+
+ return res.status(200).json({
+      response: {
+        status: 200,
+        data: file,
+      }
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
+
 
 exports.fileDelete = async (req, res) => {
   
