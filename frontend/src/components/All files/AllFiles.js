0d5834f0c931a2bcdf313e810886d93f39a0dd1d
Onuorah Ike <onuorahofficial@gmail.com>
2021-09-04 10:26:56 +0100

    updated folderCard component

--- frontend/src/components/All files/AllFiles.js
+++ frontend/src/components/All files/AllFiles.js
@@ -2,20 +2,12 @@ import Files from '../Files/Files';
 import RecentlyViewed from '../RecentlyViewed/RecentlyViewed';
 
 function AllFiles() {
-<<<<<<< HEAD
   return (
     <div>
       <RecentlyViewed />
       <Files />
     </div>
   );
-=======
-    return (
-        <div>
-            All files will be rendered here in list/grid view
-        </div>
-    )
->>>>>>> dev
 }
 
 export default AllFiles;
