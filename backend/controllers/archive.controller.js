2166723f076ab636210937bf50e514e83236d2d8
MacGee20 <giftomodu20@gmail.com>
2021-09-04 15:02:48 +0100

    feat, docs, build

--- backend/controllers/archive.controller.js
+++ backend/controllers/archive.controller.js
@@ -3,9 +3,8 @@ const { default: axios } = require('axios');
 // const generateZip = require('../middlewares/archiver');
 
 exports.archiveFile = async (req, res) => {
-        const url = await axios.patch('https://api.zuri.chat');
-        
-        const obj = url.data;
-        res.send(obj)
+        const url = await axios.patch('https://api.zuri.chat');// baseURL serves as a dummy db
+
+        res.send({url})
     };
 
--- backend/routes/archive.route.js
+++ backend/routes/archive.route.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const { archiveFile } = require('../controllers/archive.controller');
 
 
-// Routing at ...api/v1/archive/download
-
 router.get('/download', archiveFile);
 
 module.exports = router;
