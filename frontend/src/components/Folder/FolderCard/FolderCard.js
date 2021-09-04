1febaba862f39b4f77b66378c03cac13d1f01695
kenneropia <adetunmbikenny@gmail.com>
2021-09-04 14:02:15 +0100

    completed the whole folder components

--- frontend/src/components/Folder/FolderCard/FolderCard.js
+++ frontend/src/components/Folder/FolderCard/FolderCard.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisV, faFolder, faPlus } from '@fortawesome/free-solid-svg-icons';
-import FolderCardOptions from '../../FolderCardOption/FolderCardOption.js';
-import profile from './images/profile-pic.jpg';
-import classes from './FolderCard.module.css';
-const fileIcon = <FontAwesomeIcon icon={faFolder} size='3x' color='#F6BE00' />;
-const menuIcon = <FontAwesomeIcon icon={faEllipsisV} size='1x' color='#B4B4B4' />;
-const plusIcon = <FontAwesomeIcon icon={faPlus} size='sm' color='#B4B4B4' />;
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEllipsisV,
+  faFolder,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons'
+import FolderCardOptions from '../../FolderCardOption/FolderCardOption.js'
+import InviteUserToFolder from './InviteUserToFolder'
+import profile from './images/profile-pic.jpg'
+import classes from './FolderCard.module.css'
+const fileIcon = <FontAwesomeIcon icon={faFolder} size="3x" color="#F6BE00" />
+const menuIcon = (
+  <FontAwesomeIcon icon={faEllipsisV} size="1x" color="#B4B4B4" />
+)
+const plusIcon = <FontAwesomeIcon icon={faPlus} size="sm" color="#B4B4B4" />
 
 const FolderCard = () => {
-  const [menu, setMenu] = useState(false);
+  const [menu, setMenu] = useState(false)
+  const [showInvite, setShowInvite] = useState(false)
 
   const onClickMenu = () => {
-    setMenu((prev) => !prev);
-  };
+    setMenu((prev) => !prev)
+  }
   return (
     <div style={{ position: 'relative' }}>
       <div className={classes.folderCard__wrapper}>
         <div className={classes.top}>
-          <div className='cursor-pointer'>{fileIcon}</div>
+          <div className="cursor-pointer">{fileIcon}</div>
           <div className={classes.menu} onClick={() => onClickMenu()}>
             {menuIcon}
           </div>
@@ -29,15 +37,21 @@ const FolderCard = () => {
             <h6>140 files</h6>
           </div>
           <div className={classes.fileAccess}>
-            <img src={profile} alt='Profile' />
-            <img src={profile} alt='Profile' />
-            <div className={classes.add}>{plusIcon}</div>
+            <img src={profile} alt="Profile" />
+            <img src={profile} alt="Profile" />
+            <div
+              onClick={() => setShowInvite((prev) => !prev)}
+              className={classes.add}
+            >
+              {plusIcon}
+            </div>
           </div>
         </div>
       </div>
       {menu && <FolderCardOptions />}
+      {showInvite && <InviteUserToFolder />}
     </div>
-  );
-};
+  )
+}
 
-export default FolderCard;
+export default FolderCard
