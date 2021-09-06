71dc7a0987b54b2f14df8e8e98b19c00c0da1f6d
David Akin-James <davidakinjames@yahoo.com>
2021-09-06 13:36:43 +0100

    added new files modal

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,11 +2,9 @@ import React from 'react'
 import RecentlyViewed from '../RecentlyViewed/RecentlyViewed'
 import Folder from '../Folder/Folder'
 import Files from '../Files/Files'
-import SearchBar from '../SearchBar/SearchBar'
 function Home() {
   return (
     <div className="">
-      <SearchBar />
       <RecentlyViewed />
 
       <Folder />
