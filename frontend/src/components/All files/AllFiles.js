ecf2b7a4ddacac7171b8c982594b98a8a0604655
Bruno Austin <brunoaugustine2@gmail.com>
2021-09-06 01:35:03 +0100

    fix issue with href attribute in AddNewTextDoc component

--- frontend/src/components/All files/AllFiles.js
+++ frontend/src/components/All files/AllFiles.js
@@ -12,7 +12,7 @@ const AllFiles = () => {
             <div className='top flex justify-between'>
             <h2 className="text-2xl md:text-3xl">All Files</h2>
             <div className="view-actions">
-                    <button className={`view-btn list-view ${isActive === 'jsGridView' ? 'active' : ''}`} title="List View" onClick={() => setIsActive('jsGridView')}>
+                    <button className={`view-btn list-view ${isActive === 'jsGridView' ? 'active' : ''}`} title="List view" onClick={() => setIsActive('jsGridView')}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                             fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
                             strokeLinejoin="round" className="feather feather-list">
