0d687a3cc29610f41e1836406ed6acf107a97864
Ismael Tondi <smiletondi@gmail.com>
2021-09-08 18:34:20 +0000

    feat: add custom colors + add Button component

--- frontend/src/components/ComponentToTest/index.js
+++ frontend/src/components/ComponentToTest/index.js
@@ -1,6 +1,8 @@
+import Button from "Components/Button";
 import React from "react";
 
 const ComponentToTest = () => {
+  // return <Button onClick>button</Button>;
   return <h1>Test your component Here</h1>;
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,36 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: "#00B87C",
+        primary: "#1A61DB",
+        shiraz: "#B8003C",
+        text: {
+          shaft: "#242424",
+          shaftlight: "#3A3A3A",
+          dustygray: "#999999",
+          silverlight: "#C1C1C1",
+          silver: "#BEBEBE",
+        },
+        error: "#F40101",
+        wildsand: "#F6F6F6",
+        white: "#FFF",
+        success: "#008B5E",
+        avatar: {
+          pink: "#F7E0FF",
+          yellow: "#F8FFCD",
+          chablis: "#FFF0F0",
+          aeroblue: "#ACFFE6",
+          bluelight: "#E3EEFF",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
