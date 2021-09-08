51a79df429db04b4fd155840441a646604e616fb
Ismael Tondi <smiletondi@gmail.com>
2021-09-08 10:45:57 +0000

    feat: reset frontend setup
    - add prettier
    - add eslint
    - add absolute paths for easier components import
    - add basic layout

---
+++ frontend/src/components/Main/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="bg-gray-50 h-screen flex-1 flex items-center justify-center">
+      Main View
+    </div>
+  );
+};
+
+export default Main;
