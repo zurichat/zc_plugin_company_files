e9a27b8092849c1c1380f7c7d5279044ce4b5c03
Omezibe Obioha <obiohaomezibe@gmail.com>
2021-09-09 18:09:54 +0100

    updated routes/index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,12 @@
 const fileRouter = require('./file.route');
 const folderRouter = require('./folder.route');
 const archiveRouter = require('./archive.route');
+const roomsRouter = require('./rooms.router');
+const pluginRouter = require('./plugin.router');
 
 module.exports = router => {
+  router.use('/', pluginRouter);
+  router.use('/rooms', roomsRouter);
   router.use('/file', fileRouter);
   router.use('/folder', folderRouter);
   router.use('/archive', archiveRouter);
