6583ef1b68da0476100cd4baf216108614dd75c8
sephson <disutj@gmail.com>
2021-09-11 17:10:05 +0100

    get user rooms

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,15 @@
-const fileRouter = require("./file.route");
-const folderRouter = require("./folder.route");
+const fileRouter = require('./file.route');
+const folderRouter = require('./folder.route');
+const archiveRouter = require('./archive.route');
+const roomsRouter = require('./rooms.route');
+const pluginRouter = require('./plugin.router');
 
-module.exports = (router) => {
-  router.use("/file", fileRouter);
-  router.use("/folders", folderRouter);
+module.exports = router => {
+  router.use('/', pluginRouter);
+  router.use('/rooms', roomsRouter);
+  router.use('/file', fileRouter);
+  router.use('/folder', folderRouter);
+  router.use('/archive', archiveRouter);
 
   return router;
-};
+}
