2363d99bd9c2343619d4d85dab6b82de0baec5c9
Omezibe Obioha <obiohaomezibe@gmail.com>
2021-09-14 23:50:49 +0100

    Update index.js

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div className="bg-gray-50 h-screen overflow-y-scroll">
+    <div className="bg-gray-50 h-screen flex-1 flex items-center justify-center">
       <ComponentToTest />
     </div>
   );
