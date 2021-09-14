95e04fe2260979fb726dbcca9839354c715cd4c7
John Peter <jay3pee@gmail.com>
2021-09-14 09:57:57 +0100

    updated the home screen with the shortcut component

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import RecentlyViewed from "./RecentlyViewed";
 import Folder from "./Folder";
 import Files from "./Files";
+import ShortCut from "./ShortCut";
 const Index = () => {
   return (
     <div>
+      <ShortCut />
       <RecentlyViewed />
       <Folder />
       <Files />
