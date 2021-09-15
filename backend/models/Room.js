6ea1e20de996e8285265f80661657cf055be20d8
Omezibe Obioha <obiohaomezibe@gmail.com>
2021-09-15 10:34:35 +0100

    updated Room.js

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -1,29 +1,26 @@
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 const RoomSchema = Joi.object({
   ownerId: Joi.objectId().required(),
   receiverId: Joi.objectId(),
   organisationId: Joi.objectId().required(),
-  roomId: Joi.string().guid({ version: "uuidv4" }).required(),
+  roomId: Joi.string().guid({ version: 'uuidv4' }).required(),
   roomName: Joi.string().required(),
-  roomType: Joi.string()
-    .required()
-    .valid("inbox", "group", "channel", "plugin")
-    .messages({
-      "any.only": "Invalid room type! Try inbox, group, channel or plugin.",
-    }),
-  iconUrl: Joi.string().uri().default("https://zuri.chat/zurichatlogo.svg"),
-  pluginId: Joi.objectId().when("roomType", {
-    is: Joi.string().valid("plugin"),
+  roomType: Joi.string().required().valid('inbox', 'group', 'channel', 'plugin').messages({
+    'any.only': 'Invalid room type! Try inbox, group, channel or plugin.',
+  }),
+  iconUrl: Joi.string().uri().default('https://zuri.chat/zurichatlogo.svg'),
+  pluginId: Joi.objectId().when('roomType', {
+    is: Joi.string().valid('plugin'),
     then: Joi.objectId().required(),
-    otherwise: Joi.valid(null),
+    otherwise: Joi.valid(null)
   }),
   description: Joi.string().required().max(250),
   members: [Joi.objectId()],
   createdAt: Joi.date().default(new Date().toISOString()),
   isArchived: Joi.boolean().default(false),
-  isPrivate: Joi.boolean().default(true),
-});
+  isPrivate: Joi.boolean().default(true)
+})
 
 module.exports = RoomSchema;
