53a52e4dd468af15aafeebc3b85a3c6fc6d2c991
berryboylb <phemmynesce4life@gmail.com>
2021-09-16 22:43:16 +0100

    reseting the component to test folder

--- frontend/src/components/ComponentToTest/index.js
+++ frontend/src/components/ComponentToTest/index.js
@@ -1,10 +1,8 @@
 import Button from "Components/Button";
-import ImageCropper from "../Image crop/ImageCropper"
 import React from "react";
 
 const ComponentToTest = () => {
-  // return <Button onClick>button</Button>;
-   return <h1><ImageCropper /></h1> 
+  return <Button onClick>button</Button>;
 };
 
 export default ComponentToTest;
