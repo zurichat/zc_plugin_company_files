88c4efbf432692067fabdb30ce2b33020f3220f1
Ismael Tondi <smiletondi@gmail.com>
2021-09-17 12:20:19 +0000

    feat: add green bar below the search bar

--- frontend/src/components/ComponentToTest/index.js
+++ frontend/src/components/ComponentToTest/index.js
@@ -1,9 +1,7 @@
 import React from "react";
-import SearchBar from "Components/SearchBar";
 
-const ComponentToTest = () => {
-  return <SearchBar className="self-start" />;
-  // return <h1>Test your component Here</h1>;
+const ComponentToTest = (props) => {
+  return <h1>Test your component Here</h1>;
 };
 
 export default ComponentToTest;
