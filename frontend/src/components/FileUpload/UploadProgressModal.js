7a3421f07a5d8b5db14660458f27dcc776c26a72
johannesphil <jay3pee@gmail.com>
2021-09-18 09:10:44 +0100

    backed up updated file upload component and made changes to upload progress component

--- frontend/src/components/FileUpload/UploadProgressModal.js
+++ frontend/src/components/FileUpload/UploadProgressModal.js
@@ -9,7 +9,7 @@ const UploadProgressModal = ({
   return (
     <div className="absolute bottom-12 right-12 z-30">
       {progress && (
-        <div className="w-[400px] h-[380px] py-[24px] px-[26px] bg-white shadow-2xl rounded-2xl">
+        <div className="w-[420px] h-[380px] py-[24px] px-[26px] bg-white shadow-2xl rounded-2xl">
           <div className="w-full flex items-center justify-between mb-4">
             <div className="text-gray-500">
               <p>Your Uploads</p>
@@ -41,13 +41,17 @@ const UploadProgressModal = ({
                   key={file[1].name}
                 >
                   <div className="w-10/12">
-                    <span className="inline-block w-[24px] h-[24px] text-center rounded-full bg-green-700 text-white">
-                      &#x2713;
-                    </span>
-                    <span className="inline-block w-[24px] h-[24px] text-center rounded-full bg-red-600 text-white">
-                      &#10005;
-                    </span>
-                    <p className="text-[14px] truncate">{file[1].name}</p>
+                    <div className="w-full flex">
+                      <span className="inline-block w-[24px] h-[24px] text-center rounded-full bg-green-700 text-white">
+                        &#x2713;
+                      </span>
+                      <span className="inline-block w-[24px] h-[24px] mx-1 text-center rounded-full bg-red-600 text-white">
+                        &#10005;
+                      </span>
+                      <p className="w-3/4 text-[14px] truncate">
+                        {file[1].name}
+                      </p>
+                    </div>
                     <div className="w-full h-[6px] my-4 bg-green-50 rounded-full">
                       <div className="block w-[200px] h-full bg-green-400 rounded-full"></div>
                     </div>
