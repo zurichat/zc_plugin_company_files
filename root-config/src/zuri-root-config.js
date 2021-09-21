b40a4f50151327cd892d8631e7f32017471fb295
omzi <obiohaomezibe@gmail.com>
2021-09-21 14:53:30 +0100

    fix: fixed root config ish

--- root-config/src/zuri-root-config.js
+++ root-config/src/zuri-root-config.js
@@ -3,7 +3,7 @@ import { registerApplication, start } from "single-spa";
 registerApplication({
   name: '@zuri/zuri-plugin-company-files',
   app: () => System.import("@zuri/zuri-plugin-company-files"),
-  activeWhen: ["/companyfiles"]
+  activeWhen: ["/"]
 })
 
 start(
