7535f42cd9023dbe9f96867484a2890fb0d0b3be
Samuel Osinloye <psalmueloye@gmail.com>
2021-09-22 20:19:51 +0100

    folder rename

--- frontend/src/components/Home/Folder/index.js
+++ frontend/src/components/Home/Folder/index.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import folderYellow from "../../../../public/Icons/folder/yellow.svg";
 import options from "../../../../public/Icons/more-vertical/active.svg";
 import user2 from "../../../../public/Icons/folder-user2.svg";
 import user from "../../../../public/Icons/folder-user.svg";
 import addUser from "../../../../public/Icons/plus/active.svg";
+import ContextMenu from "../Files/ContextMenu";
 
 const index = () => {
+  const [revealContext, setRevealContext] = useState(false);
+  const handleContext = (e) => {
+    e.preventDefault();
+    setRevealContext(!revealContext);
+    console.log("I was clicked", revealContext);
+  };
   return (
-    <div className="w-full p-10 ">
+    <div className="w-full p-10 relative">
       <h2 className=" mb-8 text-[20px] text-gray-700">Folders</h2>
       <div className="flex flex-wrap justify-between">
         <div className="folderCard w-[235px] mb-10 px-6 py-7 flex flex-col bg-white rounded-md shadow-md">
@@ -15,9 +22,13 @@ const index = () => {
             <div className="icons">
               <img src={folderYellow} alt="" />
             </div>
-            <div className="icons">
-              <img src={options} alt="" />
-            </div>
+            {revealContext ? (
+              <ContextMenu />
+            ) : (
+              <div className="icons cursor-pointer">
+                <img src={options} alt="" onContextMenu={handleContext} />
+              </div>
+            )}
           </div>
           <div className="card text-sm flex justify-between items-center text-[13px]">
             <div className="w-32">
@@ -42,9 +53,13 @@ const index = () => {
             <div className="icons">
               <img src={folderYellow} alt="" />
             </div>
-            <div className="icons">
-              <img src={options} alt="" />
-            </div>
+            {revealContext ? (
+              <ContextMenu />
+            ) : (
+              <div className="icons cursor-pointer">
+                <img src={options} alt="" onContextMenu={handleContext} />
+              </div>
+            )}
           </div>
           <div className="card text-sm flex justify-between items-center text-[13px]">
             <div className="w-32">
@@ -69,9 +84,13 @@ const index = () => {
             <div className="icons">
               <img src={folderYellow} alt="" />
             </div>
-            <div className="icons">
-              <img src={options} alt="" />
-            </div>
+            {revealContext ? (
+              <ContextMenu />
+            ) : (
+              <div className="icons cursor-pointer">
+                <img src={options} alt="" onContextMenu={handleContext} />
+              </div>
+            )}
           </div>
           <div className="card text-sm flex justify-between items-center text-[13px]">
             <div className="w-32">
@@ -96,9 +115,13 @@ const index = () => {
             <div className="icons">
               <img src={folderYellow} alt="" />
             </div>
-            <div className="icons">
-              <img src={options} alt="" />
-            </div>
+            {revealContext ? (
+              <ContextMenu />
+            ) : (
+              <div className="icons cursor-pointer">
+                <img src={options} alt="" onContextMenu={handleContext} />
+              </div>
+            )}
           </div>
           <div className="card text-sm flex justify-between items-center text-[13px]">
             <div className="w-32">
