5a6a4b5a27971ce29d39d51cdeba31abe121cfbe
berryboylb <phemmynesce4life@gmail.com>
2021-09-25 00:17:14 +0100

    Purged css styles, added a shadow root and reduced plugin size from 13MB to 308Kb

--- frontend/src/components/VideoPreview/Index.js
+++ frontend/src/components/VideoPreview/Index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from "../Subcomponents/nav";
 import active from "../../../public/Icons/arrow-left/active.svg";
 import imageIcon from "../../../public/Icons/image/active.svg";
 import downloadIcon from "../../../public/Icons/download/active.svg";
@@ -14,32 +15,7 @@ function index({ file, setOpenStatus }) {
 
   return (
     <div className="bg-gray-800 bg-opacity-70 overflow-auto h-full w-full flex-auto flex flex-col justify-between pb-6 absolute z-10 top-0 left-0 bottom-0 right-0">
-      <nav className="h-10 w-full flex flex-row justify-between py-3 px-3 md:px-5 lg:px-7">
-        <div className="flex">
-          <div>
-            <img
-              src={active}
-              alt="arrow-left"
-              className="mr-5"
-              onClick={() => setOpenStatus(false)}
-            />
-          </div>
-          <div>
-            <div className="flex flex-row">
-              <img src={imageIcon} alt="image-icon" className="mr-2" />
-              <p className="text-white">{file.fileName}</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex">
-          <div>
-            <img src={downloadIcon} alt="download-icon" />
-          </div>
-          <div>
-            <img src={Vertical} alt="more-icon" className="ml-2" />
-          </div>
-        </div>
-      </nav>
+      <Nav file={file} setOpenStatus={setOpenStatus} />
       <div className="flex flex-col justify-between items-center w-full">
         <div className="flex justify-between w-full md:px-6 px-2">
           <div className="flex self-center">
