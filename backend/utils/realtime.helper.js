515c6e92ac8e0394b52d8a472f1827e5acfe8156
Olamide Idowu <lammy.idowu@gmail.com>
2021-09-25 20:58:46 +0100

    Update realtime.helper.js

--- backend/utils/realtime.helper.js
+++ backend/utils/realtime.helper.js
@@ -1,34 +1,28 @@
-const axios = require('axios');
-const { API_KEY, API_URL } = require('./realtime.config');
+const axios = require("axios");
+const { API_KEY, API_URL } = require("./realtime.config");
 
 class RealTime {
-    static publish = async (channel, data) => {
-
-        try {
-            
-            await axios.post(
-                API_URL,
-                {
-                    method: 'publish',
-                    params: { channel, data },
-                },
-                {
-                    headers: {
-                        'Authorization': `apikey ${API_KEY}`,
-                        'Content-Type': 'application/json'
-                    }
-                }
-
-            )
-
-            return { message:  `${channel} data has been successfully sent!`};
-
-        } catch (error) {
-            
-            return { error }
-            
+  static publish = async (channel, data) => {
+    try {
+      await axios.post(
+        API_URL,
+        {
+          method: "publish",
+          params: { channel, data },
+        },
+        {
+          headers: {
+            Authorization: `apikey ${API_KEY}`,
+            "Content-Type": "application/json",
+          },
         }
+      );
+
+      return { message: `${channel} data has been successfully sent!` };
+    } catch (error) {
+      return { error };
     }
+  };
 }
 
 module.exports = RealTime;
