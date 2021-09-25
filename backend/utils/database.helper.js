bdf6406865a5cd5c9b9b854cbef25d36b82d9af5
Olamide Idowu <lammy.idowu@gmail.com>
2021-09-25 04:30:09 +0100

    FEATURE: Archive a file

--- backend/utils/database.helper.js
+++ backend/utils/database.helper.js
@@ -3,7 +3,7 @@ const { response } = require("express");
 
 const databaseWriteUrl = "https://api.zuri.chat/data/write";
 const databaseReadUrl = "https://api.zuri.chat/data/read";
-const databaseDeleteUrl =  "https://api.zuri.chat/data/delete";
+const databaseDeleteUrl = "https://api.zuri.chat/data/delete";
 
 // const databaseWriteUrl = "https://zccore.herokuapp.com/data/write";
 // const databaseReadUrl = "https://zccore.herokuapp.com/data/read";
@@ -11,126 +11,111 @@ const databaseDeleteUrl =  "https://api.zuri.chat/data/delete";
 class DatabaseConnection {
   constructor(collection_name) {
     this.data = {
-      plugin_id: '6134c6a40366b6816a0b75cd',
-      organization_id: '6133c5a68006324323416896',
+      plugin_id: "6134c6a40366b6816a0b75cd",
+      organization_id: "6133c5a68006324323416896",
       collection_name: collection_name,
       bulk_write: false,
-      object_id: '',
+      object_id: "",
       filter: {},
-      payload: {}
+      payload: {},
     };
     this.delete_data = {
-      plugin_id: '6134c6a40366b6816a0b75cd',
-      organization_id: '6133c5a68006324323416896',
+      plugin_id: "6134c6a40366b6816a0b75cd",
+      organization_id: "6133c5a68006324323416896",
       collection_name: collection_name,
       bulk_delete: false,
-      object_id: '',
-      filter: {}
-    }
+      object_id: "",
+      filter: {},
+    };
   }
 
   create = async (payload) => {
     try {
-
       this.data.payload = payload;
-      
-      const response = await axios.post(
-        databaseWriteUrl,
-        this.data
-      );
+
+      const response = await axios.post(databaseWriteUrl, this.data);
 
       return response.data;
-
     } catch (error) {
-
       return error;
-
     }
   };
 
-  fetchAll = async ( filter = {} ) => {
-    
+  fetchAll = async (filter = {}) => {
     try {
-      
       this.data.filter = filter;
       const response = await axios.get(
         `${databaseReadUrl}/${this.data.plugin_id}/${this.data.collection_name}/${this.data.organization_id}`
       );
-  
+
       return response.data;
-
     } catch (error) {
-
       return error;
-
     }
-
   };
 
   fetchOne = async (query) => {
     try {
-      
       const response = await axios.get(
-        `${databaseReadUrl}/${this.data.plugin_id}/${this.data.collection_name}/${this.data.organization_id}?${Object.keys(query)}=${Object.values(query)}`
+        `${databaseReadUrl}/${this.data.plugin_id}/${
+          this.data.collection_name
+        }/${this.data.organization_id}?${Object.keys(query)}=${Object.values(
+          query
+        )}`
       );
-  
+
       return response.data;
-
     } catch (error) {
-      
       return error;
-
     }
   };
 
   update = async (id, payload) => {
     try {
-      
       this.data.payload = payload;
       this.data.object_id = id;
 
-      const response = await axios.put(
-        databaseWriteUrl,
-        this.data
-      );
+      const response = await axios.put(databaseWriteUrl, this.data);
 
       return response.data;
-
     } catch (error) {
-      
       return error;
-      
     }
   };
 
+  patch = async (id, payload) => {
+    try {
+      this.data.payload = payload;
+      this.data.object_id = id;
+
+      const response = await axios.patch(databaseWriteUrl, this.data);
+
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  };
 
   delete = async (id) => {
     try {
       // delete multiple files
-      let obj_id = id
+      let obj_id = id;
       if (obj_id instanceof Array) {
         let delItems = await Promise.all(
-          obj_id.map(async val => {
+          obj_id.map(async (val) => {
             this.delete_data.object_id = val;
-            let info = this.delete_data
-            let response = await axios.post(
-              databaseDeleteUrl,
-              info,
-            )
-            return response.data
-          }))
-        return delItems
-      } else {
-        this.delete_data.object_id = id
-        const response = await axios.post(
-          databaseDeleteUrl,
-          this.delete_data
+            let info = this.delete_data;
+            let response = await axios.post(databaseDeleteUrl, info);
+            return response.data;
+          })
         );
+        return delItems;
+      } else {
+        this.delete_data.object_id = id;
+        const response = await axios.post(databaseDeleteUrl, this.delete_data);
         return response.data;
       }
-
     } catch (error) {
-      
       return error;
     }
   };
