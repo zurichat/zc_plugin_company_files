b6fb89e3ba1fe10d38ebc708f22519143beb777b
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2021-10-01 12:14:23 +0100

    Updated the Starred page with endpoints

---
+++ frontend/src/components/Starred/LoadAnimation.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const LoadAnimation = () => {
+  return (
+    <div className="tw-text-center tw-mt-60 tw-preloader">
+      <div className="tw-p-3 tw-flex tw-items-center tw-text-center tw-justify-center">
+      
+       <svg class="tw-animate-bounce tw-h-4 tw-w-4 tw-mr-3 tw-bg-green-600 tw-rounded-full" viewBox="0 0 24 24">
+        </svg>
+       <svg class="tw-animate-bounce tw-h-4 tw-w-4 tw-mr-3 tw-bg-green-500 tw-rounded-full" viewBox="0 0 24 24">
+        </svg>
+       <svg class="tw-animate-bounce tw-h-3 tw-w-3 tw-mr-3 tw-bg-green-400 tw-rounded-full" viewBox="0 0 24 24">
+        </svg>
+       <svg class="tw-animate-bounce tw-h-2 tw-w-2 tw-mr-3 tw-bg-green-300 tw-rounded-full" viewBox="0 0 24 24">
+        </svg>
+       <svg class="tw-animate-bounce tw-h-1 tw-w-1 tw-mr-3 tw-bg-green-200 tw-rounded-full" viewBox="0 0 24 24">
+        </svg>
+             </div>
+     
+    </div>
+  );
+}
+
+export default LoadAnimation;
