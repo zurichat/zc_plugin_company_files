8cae51daf0e2e11e78f4942332a06aa5385262bb
sephson <disutj@gmail.com>
2021-10-03 00:26:17 +0100

    corr

--- frontend/src/components/RecentlyViewed/RecentlyViewedImg/RecentlyViewedImg.js
+++ frontend/src/components/RecentlyViewed/RecentlyViewedImg/RecentlyViewedImg.js
@@ -1,63 +1,78 @@
-import React from 'react'
-import { BsArrowUpDown } from "react-icons/bs";
-import { BsGrid3X2 } from "react-icons/bs";
-import {Link} from "react-router-dom";
-import classes from './RecentlyViewedImg.module.css'
+// import React, { useState, useEffect } from 'react'
+// import { BsArrowUpDown } from "react-icons/bs";
+// import { BsGrid3X2 } from "react-icons/bs";
+// import {Link} from "react-router-dom";
+// import axios from 'axios'
+// import ImageIcon from '../svg/imageIcon';
+// import classes from '../RecentlyViewed.module.css'
 
-function RecentlyViewedImg() {
-    const goBack = () => {
-        const currentState = history.state;
-        history.pushState(currentState, '', '/companyfiles');
-    }
+// function RecentlyViewedImg() {
+//     const [images, setImages] = useState([])
+    
+//     useEffect(() => {
+//         axios.get('https://companyfiles.zuri.chat/api/v1/files/recentlyViewedImages')
+//         .then(res => {
+//             console.log('res', res.data)
+//             setImages(res.data)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+//     }, [])
+//     const goBack = () => {
+//         const currentState = history.state;
+//         history.pushState(currentState, '', '/companyfiles');
+//     }
 
-    const backBtn = (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M20 12H4" stroke="#333333" stroke-width="1.22693" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M10 18L4 12L10 6" stroke="#333333" stroke-width="1.22693" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-    )
+//     const backBtn = (
+//         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+//             <path d="M20 12H4" stroke="#333333" stroke-width="1.22693" stroke-linecap="round" stroke-linejoin="round"/>
+//             <path d="M10 18L4 12L10 6" stroke="#333333" stroke-width="1.22693" stroke-linecap="round" stroke-linejoin="round"/>
+//         </svg>
+//     )
 
-    return (
-        <div className={classes.recentlyViewedImg}>
-            <div className={classes.header}>
-                <div className={classes.left}>
-                    <div onClick={goBack}>
-                        {backBtn}
-                    </div>
-                    <span>Images</span>
-                </div>
-                <div className={classes.right}>
-                    <BsArrowUpDown title="sort" className="tw-text-gray-400 tw-text-lg tw-mx-2 hover:tw-text-gray-500 tw-cursor-pointer" />
-                    <BsGrid3X2 title="grid" className="tw-text-gray-400 tw-mx-2 tw-text-2xl hover:tw-text-gray-500 tw-cursor-pointer" />
-                    <Link
-                        to="/activities"
-                        className="tw-mx-4 tw-py-2 tw-px-4 tw-bg-green-500 tw-text-white tw-text-sm tw-rounded hover:tw-bg-green-600"
-                    >
-                        See Activities
-                    </Link>
-                </div>
-            </div>
-            <div className={classes.body}>
-                {
-                    images.map((image, idx) => (
-                        <div className={classes.container}>
-                            <div className={classes.icon} style={{background: '#E3EEFF'}}>
-                                <ImageIcon />
-                            </div>
-                            <div className={classes.fileDetails}>
-                                <div className={classes.fileName}>
-                                    {image.fileName}
-                                </div>
-                                <div className={classes.timeStamp}>
-                                    {format(image.lastAccessed)}
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
-        </div>
-    )
-}
+//     console.log(images)
+//     return (
+//         <div className={classes.recentlyViewed}>
+//             <div className={classes.header}>
+//                 <div className={classes.left}>
+//                     <div onClick={goBack}>
+//                         {backBtn}
+//                     </div>
+//                     <span>Recently Viewed Images</span>
+//                 </div>
+//                 <div className={classes.right}>
+//                     <BsArrowUpDown title="sort" className="tw-text-gray-400 tw-text-lg tw-mx-2 hover:tw-text-gray-500 tw-cursor-pointer" />
+//                     <BsGrid3X2 title="grid" className="tw-text-gray-400 tw-mx-2 tw-text-2xl hover:tw-text-gray-500 tw-cursor-pointer" />
+//                     <Link
+//                         to="/activities"
+//                         className="tw-mx-4 tw-py-2 tw-px-4 tw-bg-green-500 tw-text-white tw-text-sm tw-rounded hover:tw-bg-green-600"
+//                     >
+//                         See Activities
+//                     </Link>
+//                 </div>
+//             </div>
+//             <div className={classes.body}>
+//                 {
+//                     images.map((image, idx) => (
+//                         <div className={classes.container}>
+//                             <div className={classes.icon} style={{background: '#E3EEFF'}}>
+//                                 <ImageIcon />
+//                             </div>
+//                             <div className={classes.fileDetails}>
+//                                 <div className={classes.fileName}>
+//                                     {image.fileName}
+//                                 </div>
+//                                 <div className={classes.timeStamp}>
+//                                     {image.lastAccessed}
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     ))
+//                 }
+//             </div>
+//         </div>
+//     )
+// }
 
-export default RecentlyViewedImg
+// export default RecentlyViewedImg
