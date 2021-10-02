b4e09bd42f5f2944154d6f5ade37555407f1a2ee
Damilola Akinlade <akinlade.co@gmail.com>
2021-10-02 21:15:11 +0100

    feat:-recently-viewed-files

--- frontend/src/components/RecentlyViewed/RecentlyViewedImg/RecentlyViewedImg.js
+++ frontend/src/components/RecentlyViewed/RecentlyViewedImg/RecentlyViewedImg.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BsArrowUpDown } from "react-icons/bs";
 import { BsGrid3X2 } from "react-icons/bs";
 import {Link} from "react-router-dom";
-import classes from './RecentlyViewedImg.module.css'
+import axios from 'axios'
+import ImageIcon from '../svg/imageIcon';
+import classes from '../RecentlyViewed.module.css'
 
 function RecentlyViewedImg() {
+    const [images, setImages] = useState([])
+    
+    useEffect(() => {
+        axios.get('https://companyfiles.zuri.chat/api/v1/files/recentlyViewedImages')
+        .then(res => {
+            console.log('res', res.data)
+            setImages(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
     const goBack = () => {
         const currentState = history.state;
         history.pushState(currentState, '', '/companyfiles');
@@ -17,14 +31,15 @@ function RecentlyViewedImg() {
         </svg>
     )
 
+    console.log(images)
     return (
-        <div className={classes.recentlyViewedImg}>
+        <div className={classes.recentlyViewed}>
             <div className={classes.header}>
                 <div className={classes.left}>
                     <div onClick={goBack}>
                         {backBtn}
                     </div>
-                    <span>Images</span>
+                    <span>Recently Viewed Images</span>
                 </div>
                 <div className={classes.right}>
                     <BsArrowUpDown title="sort" className="tw-text-gray-400 tw-text-lg tw-mx-2 hover:tw-text-gray-500 tw-cursor-pointer" />
@@ -38,7 +53,23 @@ function RecentlyViewedImg() {
                 </div>
             </div>
             <div className={classes.body}>
-
+                {
+                    images.map((image, idx) => (
+                        <div className={classes.container}>
+                            <div className={classes.icon} style={{background: '#E3EEFF'}}>
+                                <ImageIcon />
+                            </div>
+                            <div className={classes.fileDetails}>
+                                <div className={classes.fileName}>
+                                    {image.fileName}
+                                </div>
+                                <div className={classes.timeStamp}>
+                                    {image.lastAccessed}
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
