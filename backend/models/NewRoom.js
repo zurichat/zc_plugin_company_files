8edd1a87cba2630fd61e33e029cc0d0b015584a4
Ekeyekwu Oscar Ikechukwu <oscarekeyekwu@gmail.com>
2021-10-02 05:45:56 +0100

    updated room schema

--- backend/models/NewRoom.js
+++ backend/models/NewRoom.js
@@ -5,9 +5,12 @@ const NewRoomSchema = Joi.object({
   room_url: Joi.string().required(),
   room_image: Joi.string().uri().default('https://zuri.chat/zurichatlogo.svg'),
   isDefault: Joi.boolean().default(false),
-  isPrivate: Joi.boolean().default(false),
+  private: Joi.boolean().default(false),
   org_id: Joi.string().required(),
-  members: Joi.array().default([])
+  room_member_ids: Joi.array().default([]),
+  room_created_by: Joi.string(),
+  room_created_at: Joi.date().default(new Date()),
+  room_modified_at: Joi.date().default(new Date()),
 });
 
 module.exports = NewRoomSchema;
