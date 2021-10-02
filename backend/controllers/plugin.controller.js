52ccb204825e8321c1c50dc97dd0e1503b0b4100
Ekeyekwu Oscar Ikechukwu <oscarekeyekwu@gmail.com>
2021-10-02 11:19:19 +0100

    revert check for current user

--- backend/controllers/plugin.controller.js
+++ backend/controllers/plugin.controller.js
@@ -48,7 +48,7 @@ exports.sidebar = async (req, res) => {
 
   let data = await Rooms.fetchAll({org_id: org});
   handleRoomUrl(data);
-  const defaultRooms = data.filter(room => room.isDefault && room.room_member_ids.includes(user))
+  const defaultRooms = data.filter(room => room.isDefault)
   console.log(req.session)
   .map(({ room_name, room_url, room_image }) => ({ room_name, room_url, room_image }));
  
