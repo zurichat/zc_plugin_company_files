d7dc4e0ec144bca1584a2e8f0977638dc5a01a83
Williams Chukwuebuka Malachy <billmal071@gmail.com>
2021-10-02 21:55:04 +0100

    Revert "dispatch fixed"

--- frontend/src/actions/fileAction.js
+++ frontend/src/actions/fileAction.js
@@ -49,22 +49,18 @@ export const fileDetails = (id) => async (dispatch) => {
 
 export const deleteFile = (file) => async (dispatch) => {
   try {
-    dispatch({ type: 'DELETE_FILE_FULFILLED', payload: file._id });
     const response = await axios.put(`/files/deleteToBin/${file._id}`);
+    console.log(response);
   } catch (error) {
     console.log(error);
   }
 };
 
 export const starFile = (file) => async (dispatch) => {
-  const newFile = {
-    ...file,
-    isStarred: true,
-  };
-
+  console.log(file._id);
   try {
-    dispatch({ type: 'STAR_FILE_FULFILLED', payload: newFile });
     const response = await axios.put(`/files/starFile/${file._id}`, { isStarred: true });
+    console.log(response);
   } catch (error) {
     console.log(error);
   }
