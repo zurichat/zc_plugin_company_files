325ab6f7725ce86cb03693d13d8d11705f9a8815
Onuorah Ike <onuorahofficial@gmail.com>
2021-10-02 22:34:08 +0100

    updated reducer and action files

--- frontend/src/actions/fileAction.js
+++ frontend/src/actions/fileAction.js
@@ -20,7 +20,6 @@ export function fetchFiles() {
   };
 }
 
-
 export function setLoading() {
   return {
     type: 'FETCH_FILES_PENDING' || 'ADD_FILE_PENDING' || 'DELETE_FILE_PENDING' || 'UPDATE_FILE_PENDING',
@@ -37,7 +36,6 @@ export const checkRecentlyViewed = (id) => async (dispatch) => {
 };
 
 export const fileDetails = (id) => async (dispatch) => {
-
   console.log(file._id);
   try {
     const response = await axios.post(`/files/read/${id}`);
@@ -49,18 +47,22 @@ export const fileDetails = (id) => async (dispatch) => {
 
 export const deleteFile = (file) => async (dispatch) => {
   try {
+    dispatch({ type: 'DELETE_FILE_FULFILLED', payload: file._id });
     const response = await axios.put(`/files/deleteToBin/${file._id}`);
-    console.log(response);
   } catch (error) {
     console.log(error);
   }
 };
 
 export const starFile = (file) => async (dispatch) => {
-  console.log(file._id);
+  const newFile = {
+    ...file,
+    isStarred: true,
+  };
+
   try {
+    dispatch({ type: 'STAR_FILE_FULFILLED', payload: newFile });
     const response = await axios.put(`/files/starFile/${file._id}`, { isStarred: true });
-    console.log(response);
   } catch (error) {
     console.log(error);
   }
