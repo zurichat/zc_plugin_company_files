53c05a0963f114c59b59271891fd4e1fa9abeacf
Omezibe Obioha <obiohaomezibe@gmail.com>
2021-10-11 21:14:15 +0100

    Revert "feat-search_endpoint"

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const { searchAndFilterFiles,searchFileAndFolder,testSearch } = require('../controllers/search.controller');
+const { searchAndFilterFiles } = require('../controllers/search.controller');
 
 router.get('/', searchAndFilterFiles);
-router.get('/fileAndFolder/', searchFileAndFolder)
-router.get('/testSearch/', testSearch)
 
 module.exports = router;
