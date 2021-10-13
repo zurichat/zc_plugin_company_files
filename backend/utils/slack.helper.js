15b192d5054c11e8a3da5e017d66ef8861308077
Omezibe Obioha <obiohaomezibe@gmail.com>
2021-10-13 09:52:10 +0100

    fix: changed Slack webhook

--- backend/utils/slack.helper.js
+++ backend/utils/slack.helper.js
@@ -1,4 +1,4 @@
 exports.config = {
-    url: "https://hooks.slack.com/services/",
-    webhook: "T02HBS55FCG/B02HAJFPDU5/B9DFC7gtk3Xw2DASp0KRm5Xm"
-};
+  url: "https://hooks.slack.com/services/",
+  webhook: "T02HBS55FCG/B02HLJCESGJ/MHRyEeLOPIHFXghN2m1wXjh6"
+}
