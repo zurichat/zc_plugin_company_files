71dc1ee0c3de831a422b310018ce4957b120dfc0
omzi <obiohaomezibe@gmail.com>
2021-10-13 08:25:45 +0100

    feat: added fetchByFilter() method

--- backend/utils/database.helper.js
+++ backend/utils/database.helper.js
@@ -40,18 +40,17 @@ class DatabaseOps {
   create = async (payload) => {
     this.data.filter = undefined;
     this.data.payload = payload;
-    const response = await axios.post(databaseWriteUrl, this.data);
+    const { data } = await axios.post(databaseWriteUrl, this.data);
 
-    return response.data;
+    return data;
   }
 
-  fetchAll = async (filter = {}) => {
-    this.data.filter = this.normalizeFilterQuery(filter);
+  fetchAll = async () => {
     const { data } = await axios.get(
       `${databaseReadUrl}/${this.data.plugin_id}/${this.data.collection_name}/${this.data.organization_id}`
     );
 
-    return data;
+    return data === null ? [] : data;
   }
 
   fetchOne = async (query) => {
@@ -60,6 +59,14 @@ class DatabaseOps {
     );
 
     return data;
+  }
+
+  fetchByFilter = async (filter = {}) => {
+    this.data.filter = filter;
+    
+    const { data } = await axios.post(`${databaseReadUrl}`, this.data);
+
+    return data === null ? [] : data;
   }
 
   update = async (id, payload) => {
