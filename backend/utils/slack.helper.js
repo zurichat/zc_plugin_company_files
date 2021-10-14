cad987f6767672de6617a744dfec2ac2c9e94f59
tracetechie <ismailabdulqudus063@gmail.com>
2021-10-14 08:38:46 +0800

    fix: fixed the slack push notification service bugs.

--- backend/utils/slack.helper.js
+++ backend/utils/slack.helper.js
@@ -1,4 +1,6 @@
 exports.config = {
     url: "https://hooks.slack.com/services/",
-    webhook: "T02HBS55FCG/B02HAJFPDU5/B9DFC7gtk3Xw2DASp0KRm5Xm"
+    // webhook: "T02HBS55FCG/B02H7P0B7N3/ZNusd3sNVBz594jkJoeqOEnx", // Devvyhac's DM
+    webhook: "T02HBS55FCG/B02HSPYF6G4/U1768xUG1CNVn9V64slxATT6" // #notifications channel
 };
+
