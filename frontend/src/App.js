323ecf5daa3d9eff9b33de88a4d4aa2982aab4d0
billmal071 <billmal071@gmail.com>
2021-10-31 00:47:22 +0100

    fix:resolved start issues and added eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+
 import Layout from "./components/Layout";
 import Main from "./components/Main";
-import React, { useEffect } from "react";
 import "./App.css";
-import axios from "axios";
 import { getUserInfo } from "./actions/workspaceInfo";
-import { useDispatch, useSelector } from "react-redux";
 import store from "./store/store";
 
 const API_URL =
@@ -16,12 +17,24 @@ const API_URL =
 axios.defaults.baseURL = API_URL;
 const info = store.getState().rootReducer.workspaceReducer.info;
 axios.defaults.headers.common["Authorization"] = `Bearer ${info?.token}`;
-axios.defaults.headers.userObj = {
-  userName: typeof info === "object" && info !== null && info !== {} && info[0]?.user_name,
-  imageUrl: typeof info === "object" && info !== null && info !== {} && info[0]?.img_url,
-  userId: typeof info === "object" && info !== null && info !== {} && info[0]?._id,
+axios.defaults.headers.userObject = {
+  userName:
+    typeof info === "object" &&
+    info !== null &&
+    info !== {} &&
+    info[0]?.user_name,
+  imageUrl:
+    typeof info === "object" &&
+    info !== null &&
+    info !== {} &&
+    info[0]?.img_url,
+  userId:
+    typeof info === "object" && info !== null && info !== {} && info[0]?._id,
+  userEmail:
+    typeof info === "object" && info !== null && info !== {} && info[0]?.email,
+  userOrgId:
+    typeof info === "object" && info !== null && info !== {} && info[0]?.org_id
 };
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 export default function Home() {
