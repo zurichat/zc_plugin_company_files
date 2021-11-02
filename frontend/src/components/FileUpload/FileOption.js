54fd05f068afec2ab1374d27a65fa78a3739550d
billmal071 <billmal071@gmail.com>
2021-11-02 16:11:25 +0100

    test:husky precommit

--- frontend/src/components/FileUpload/FileOption.js
+++ frontend/src/components/FileUpload/FileOption.js
@@ -5,7 +5,7 @@ function FileOption({ image, altText, name, cmd }) {
   return (
     <div
       className="tw-w-full tw-px-5 tw-h-8 tw-my-2 tw-text-[14px] tw-cursor-pointer tw-text-gray-400 hover:tw-bg-green-500 hover:tw-text-white tw-flex tw-items-center"
-      onClick={() => cmd()}
+      onClick={cmd}
     >
       <img src={image} alt={altText} />
       <span className="tw-ml-4">{name}</span>
