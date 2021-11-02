4d0aaa38150299c1b2064bc558057b5bf44ea256
billmal071 <billmal071@gmail.com>
2021-11-02 14:46:08 +0100

    test:husky precommit

--- frontend/src/components/FileUpload/index.jsx
+++ frontend/src/components/FileUpload/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import SelectFileModal from "./SelectFileModal";
 import UploadProgressModal from "./UploadProgressModal";
 
